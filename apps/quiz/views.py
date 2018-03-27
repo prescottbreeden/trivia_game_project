5f156671f8f9539f6919ec5efa9cb0bb46d954fb
prescott breeden <pb@prescotts-iMac.local>
2018-03-27 00:57:11 -0700

    Added CSS3 and animations
    needs more work, just did basics for quiz cat selection and quizes

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -6,19 +6,14 @@ from apps.login.models import *
 def quiz(request):
 	if 'user_id' not in request.session:
 		return redirect('/')
-	context = {
-		'categories' : Category.objects.all()
-		}
-	
+	context = {'categories' : Category.objects.all()}
 	return render(request, 'quiz/quiz.html', context)
 
 def create_quiz(request, id):
 	if 'user_id' not in request.session:
 		return redirect('/')
 	
-	context = {
-		'questions' : Quiz.objects.make_quiz(id = id)
-	}
+	context = {'questions' : Quiz.objects.make_quiz(id = id)}
 	return render(request, 'quiz/take_quiz.html', context)
 
 def quiz_end(request):
